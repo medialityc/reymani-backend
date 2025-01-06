a40cb755c788b3a9b67b530ae43bcce558f84e24
rafaelrdgz <rafaelrdgz2003@gmail.com>
2025-01-06 12:40:19 -0500

    feat(direccion): added update direccion endpoint

---
+++ Api/Endpoints/Direccion/UpdateDireccionEndpoint.cs
@@ -0,0 +1,107 @@
+using System;
+using reymani_web_api.Domain.Enums;
+
+namespace reymani_web_api.Api.Endpoints.Direccion;
+
+public class UpdateDireccionEndpoint : Endpoint<UpdateDireccionRequest>
+{
+  private readonly IDireccionService _direccionService;
+  private readonly IAuthorizationService _authorizationService;
+  private readonly IClienteService _clienteService;
+  private readonly INegocioService _negocioService;
+
+  public UpdateDireccionEndpoint(IDireccionService direccionService, IAuthorizationService authorizationService, IClienteService clienteService, INegocioService negocioService)
+  {
+    _direccionService = direccionService;
+    _authorizationService = authorizationService;
+    _clienteService = clienteService;
+    _negocioService = negocioService;
+  }
+
+  public override void Configure()
+  {
+    Verbs(Http.PUT);
+    Routes("/direccion/{IdDireccion:guid}");
+    Summary(s =>
+    {
+      s.Summary = "Actualizar Dirección";
+      s.Description = "Actualiza una dirección en la base de datos";
+      s.ExampleRequest = new UpdateDireccionRequest
+      {
+        IdDireccion = Guid.NewGuid(),
+        Direccion = new Domain.Entities.Direccion
+        {
+          DireccionEntidad = "Calle Falsa 123",
+          IdEntidad = Guid.NewGuid(),
+          Descripcion = "Dirección de ejemplo",
+          TipoEntidad = "Negocio",
+          Municipio = "Springfield",
+          Provincia = "Springfield",
+          Latitud = 0.0,
+          Longitud = 0.0
+        }
+      };
+    });
+  }
+
+  public override async Task HandleAsync(UpdateDireccionRequest req, CancellationToken ct)
+  {
+    var roleGuids = User.Claims.Where(c => c.Type == "role").Select(c => Guid.Parse(c.Value)).ToArray();
+
+    if (!await _authorizationService.IsRoleAuthorizedToEndpointAsync(roleGuids, "Actualizar_Direccion"))
+    {
+      await SendUnauthorizedAsync(ct);
+    }
+
+    var direccion = await _direccionService.GetByIdAsync(req.IdDireccion);
+    if (direccion == null)
+    {
+      await SendNotFoundAsync(ct);
+      return;
+    }
+
+    if (req.Direccion.IdDireccion != req.IdDireccion)
+    {
+      AddError(r => r.IdDireccion, "El ID de la dirección no coincide con el ID de la URL");
+    }
+
+    if (!Enum.TryParse(typeof(EntitiesTypes), req.Direccion.TipoEntidad, true, out var tipoEntidad))
+    {
+      AddError(r => r.Direccion.TipoEntidad, "Tipo de entidad inválido");
+    }
+
+    ThrowIfAnyErrors();
+
+    if (tipoEntidad != null && (EntitiesTypes)tipoEntidad == EntitiesTypes.Cliente)
+    {
+      var cliente = await _clienteService.GetClienteByIdAsync(req.Direccion.IdEntidad);
+      if (cliente == null)
+      {
+        AddError(r => r.Direccion.IdEntidad, "Cliente no encontrado");
+      }
+    }
+    else if (tipoEntidad != null && (EntitiesTypes)tipoEntidad == EntitiesTypes.Negocio)
+    {
+      var negocio = await _negocioService.GetByIdAsync(req.Direccion.IdEntidad);
+      if (negocio == null)
+      {
+        AddError(r => r.Direccion.IdEntidad, "Negocio no encontrado");
+      }
+    }
+
+    ThrowIfAnyErrors();
+
+    if (direccion != null)
+    {
+      direccion.DireccionEntidad = req.Direccion.DireccionEntidad;
+      direccion.IdEntidad = req.Direccion.IdEntidad;
+      direccion.Descripcion = req.Direccion.Descripcion;
+      direccion.Municipio = req.Direccion.Municipio;
+      direccion.Provincia = req.Direccion.Provincia;
+      direccion.Latitud = req.Direccion.Latitud;
+      direccion.Longitud = req.Direccion.Longitud;
+      await _direccionService.UpdateAsync(direccion);
+      await SendOkAsync(ct);
+    }
+  }
+}
