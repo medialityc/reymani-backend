45091261f311a26028a7e25dac93e63870d401ff
rafaelrdgz <rafaelrdgz2003@gmail.com>
2025-01-08 14:56:47 -0500

    feat(categoria negocio): added get categoria negocio by id endpoint

---
+++ Api/Endpoints/CategoriaNegocio/GetCategoriaNegocioByIdRequest.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace reymani_web_api.Api.Endpoints.CategoriaNegocio
+{
+  public class GetCategoriaNegocioByIdRequest
+  {
+    public Guid CategoriaNegocioId { get; set; }
+  }
+}
