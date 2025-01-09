596f7aa8de5e4a23c95bfab1264b7147f15f39ff
rafaelrdgz <rafaelrdgz2003@gmail.com>
2025-01-09 11:07:21 -0500

    feat(costo envio): added delete costo envio endpoint

---
+++ Api/Endpoints/CostoEnvio/DeleteCostoEnvioRequest.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace reymani_web_api.Api.Endpoints.CostoEnvio;
+
+public class DeleteCostoEnvioRequest
+{
+  public Guid CostoEnvioId { get; set; }
+}
--- Api/Endpoints/Rol/DeleteRolRequest.cs
+++ Api/Endpoints/Rol/DeleteRolRequest.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace reymani_web_api.Api.Endpoints.Rol;
 
 public class DeleteRolRequest
