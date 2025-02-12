61e573c1774a4277c3af603f06a6a91924c9d9b8
rafaelrdgz <rafaelrdgz2003@gmail.com>
2025-02-12 13:56:09 -0500

    feat(productcategories): added update endpoint

--- Endpoints/ProductCategories/Requests/Validators/UpdateProductCategoryRequestValidator.cs
+++ Endpoints/ProductCategories/Requests/Validators/UpdateProductCategoryRequestValidator.cs
@@ -6,11 +6,17 @@ using reymani_web_api.Utils.Validations;
 
 namespace reymani_web_api.Endpoints.ProductCategories.Requests.Validators
 {
-  public class CreateProductCategoryRequestValidator : Validator<CreateProductCategoryRequest>
+  public class UpdateProductCategoryRequestValidator : Validator<UpdateProductCategoryRequest>
   {
-    public CreateProductCategoryRequestValidator()
+    public UpdateProductCategoryRequestValidator()
     {
+      RuleFor(x => x.Id)
+        .GreaterThan(0);
+
       RuleFor(x => x.Name).NotEmpty();
+
+      RuleFor(x => x.IsActive)
+        .NotNull();
 
       RuleFor(x => x.Logo)
         .Must(file => file == null || (ImageValidations.BeAValidImage(file) && ImageValidations.HaveValidLength(file)));
