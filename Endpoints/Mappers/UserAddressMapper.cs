9e73c38505643d0aa9ab5d06c098903fdc64d2ae
Angel Hernandez <LuXuzStudios@gmail.com>
2025-02-21 09:12:02 -0500

    feat(AddressUser): GelAllCustomers
    -Creado el mapper de user address

---
+++ Endpoints/Mappers/UserAddressMapper.cs
@@ -0,0 +1,24 @@
+﻿using reymani_web_api.Endpoints.AddressUser.Responses;
+using reymani_web_api.Endpoints.Municipalities.Responses;
+using reymani_web_api.Endpoints.Provinces.Requests;
+using reymani_web_api.Endpoints.Provinces.Responses;
+using ReymaniWebApi.Data.Models;
+
+namespace reymani_web_api.Endpoints.Mappers;
+
+public class UserAddressMapper
+{
+  public UserAddressResponse FromEntity(UserAddress e)
+  {
+    return new UserAddressResponse
+    {
+      Id = e.Id,
+      Address = e.Address,
+      Name = e.Name,
+      ProvinceId = e.Municipality.Province.Id,
+      ProvinceName =e.Municipality.Province.Name,
+      MunicipalityId = e.MunicipalityId,
+      MunicipalityName = e.Municipality?.Name ?? ""
+    };
+  }
+}
